267f3413d5b25efa27adb7562e0a9c527fb29d10
Shantalya <86602366+Wolkenschutz@users.noreply.github.com>
2024-06-05 18:34:28 +0200

    Improvement: The options icon has the new design from TWW.

--- xui/options_button.lua
+++ xui/options_button.lua
@@ -3,14 +3,6 @@ local AddonName, KeystoneLoot = ...;
 local Translate = KeystoneLoot.Translate;
 
 
-local function OnEnter(self) -- Kann weg mit TWW
-	self:SetAlpha(1);
-end
-
-local function OnLeave(self) -- Kann weg mit TWW
-	self:SetAlpha(0.7);
-end
-
 local function OnClick(self)
 	KeystoneLoot:ToggleDropDown(self);
 
@@ -22,12 +14,12 @@ end
 
 local function OnMouseDown(self)
 	self.GearTexture:SetPoint('CENTER', 1, -1);
-	--self.HighlightTexture:SetPoint('CENTER', 1, -1);
+	self.HighlightTexture:SetPoint('CENTER', 1, -1);
 end
 
 local function OnMouseUp(self)
 	self.GearTexture:SetPoint('CENTER');
-	--self.HighlightTexture:SetPoint('CENTER');
+	self.HighlightTexture:SetPoint('CENTER');
 end
 
 local function OnShow(self)
@@ -38,23 +30,30 @@ local OverviewFrame = KeystoneLoot:GetOverview();
 
 local Button = CreateFrame('Button', nil, OverviewFrame);
 OverviewFrame.OptionsButton = Button;
-Button:SetAlpha(0.7); -- Kann weg mit TWW
 Button:SetSize(18, 18);
 Button:SetFrameLevel(510);
 Button:RegisterForClicks('LeftButtonUp', 'RightButtonUp');
 Button:SetPoint('TOPRIGHT', -28, -3);
 Button:SetScript('OnShow', OnShow);
-Button:SetScript('OnEnter', OnEnter); -- Kann weg mit TWW
-Button:SetScript('OnLeave', OnLeave); -- Kann weg mit TWW
 Button:SetScript('OnClick', OnClick);
 Button:SetScript('OnMouseDown', OnMouseDown);
 Button:SetScript('OnMouseUp', OnMouseUp);
 
 local GearTexture = Button:CreateTexture(nil, 'ARTWORK');
 Button.GearTexture = GearTexture;
-GearTexture:SetSize(18, 18);
+GearTexture:SetSize(15, 16);
 GearTexture:SetPoint('CENTER');
-GearTexture:SetTexture('Interface\\WorldMap\\GEAR_64GREY');
+GearTexture:SetTexture('Interface\\AddOns\\'..AddonName..'\\assets\\QuestlogFrame2x');
+GearTexture:SetTexCoord(0.932617, 0.961914, 0.0322266, 0.0634766);
+
+local HighlightTexture = Button:CreateTexture(nil, 'HIGHLIGHT');
+Button.HighlightTexture = HighlightTexture;
+HighlightTexture:SetSize(15, 16);
+HighlightTexture:SetPoint('CENTER');
+HighlightTexture:SetTexture('Interface\\AddOns\\'..AddonName..'\\assets\\QuestlogFrame2x');
+HighlightTexture:SetTexCoord(0.932617, 0.961914, 0.0322266, 0.0634766);
+HighlightTexture:SetBlendMode('ADD');
+HighlightTexture:SetAlpha(0.4);
 
 -- local GearTexture = Button:CreateTexture(nil, 'ARTWORK');
 -- Button.GearTexture = GearTexture;
