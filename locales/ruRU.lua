90859f7fb97385be1dfaea8f02ccbdf85fd8d9c5
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2026-04-23 01:53:17 +0500

    Update ruRU.lua

--- locales/ruRU.lua
+++ locales/ruRU.lua
@@ -51,7 +51,7 @@ L["No character selected."] = "Персонаж не выбран.";
 L["No valid items found."] = "Допустимые предметы не найдены.";
 
 -- icon_button.lua / favorites.lua
-L["Set Favorite"] = "Установить избранное";
+L["Set Favorite"] = "Добавить в избранное";
 L["Nice to have"] = "Желательно";
 L["Must have"] = "Обязательно";
 L["Best in Slot"] = "БиС";
